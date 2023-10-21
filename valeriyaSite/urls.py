dc721a9c21084408f620c8775765dff9db4b0218
Vlad <belouskovlad@gmail.com>
2023-10-21 17:13:13 +0600

    повтор собрания статики

--- valeriyaSite/urls.py
+++ valeriyaSite/urls.py
@@ -24,4 +24,8 @@ urlpatterns = [
     path('', include('main.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('bot/', include('bot.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
